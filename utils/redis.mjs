34ed53472418828afcf255bd0b7334972be2e6cc
ojvl1 <antompietriosvaldo@gmail.com>
2024-11-29 13:55:10 -0400

    .

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -5,33 +5,34 @@ class RedisClient {
     this.client = createClient();
 
     this.client.on("error", (err) => {
-      console.error("Redis Client Error", err);
+      console.error("Redis client error:", err);
     });
 
-    this.client.connect().catch(console.error);
+    this.client.connect().catch((err) => {
+      console.error("Failed to connect to Redis:", err);
+    });
   }
 
   isAlive() {
-    return this.client.isOpen;
+    return this.client.isReady;
   }
 
   async get(key) {
     try {
-      const value = await this.client.get(key);
-      return value;
+      return await this.client.get(key);
     } catch (err) {
-      console.error("Error getting key", err);
+      console.error("Error getting value from Redis:", err);
       return null;
     }
   }
 
   async set(key, value, duration) {
     try {
-      await this.client.set(key, value.toString(), {
+      await this.client.set(key, value, {
         EX: duration,
       });
     } catch (err) {
-      console.error("Error setting key", err);
+      console.error("Error setting value in Redis:", err);
     }
   }
 
@@ -39,11 +40,10 @@ class RedisClient {
     try {
       await this.client.del(key);
     } catch (err) {
-      console.error("Error deleting key", err);
+      console.error("Error deleting value from Redis:", err);
     }
   }
 }
 
 const redisClient = new RedisClient();
-
 export default redisClient;
