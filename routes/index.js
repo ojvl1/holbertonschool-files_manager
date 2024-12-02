84479e49e5334e2f4b6579664e6996486d1b62b5
ojvl1 <antompietriosvaldo@gmail.com>
2024-12-01 20:06:21 -0400

    First file

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,18 @@ import express from "express";
 import AppController from "../controllers/AppController.js";
 import AuthController from "../controllers/AuthController.js";
 import UserController from "../controllers/UserController.js";
+import FilesController from "../controllers/FilesController.js";
 
 const router = express.Router();
 
 // Existing endpoints
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
-
-// New endpoints
 router.get("/connect", AuthController.getConnect);
 router.get("/disconnect", AuthController.getDisconnect);
 router.get("/users/me", UserController.getMe);
+
+// New endpoint
+router.post("/files", FilesController.postUpload);
 
 export default router;
